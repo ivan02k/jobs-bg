37e7671aa51d0aab272e6928c31b687bb465b840
Ivan I <Ivan I@DESKTOP-F2FA450>
2023-01-16 11:40:36 +0200

    Routes

--- routes/web.php
+++ routes/web.php
@@ -13,13 +13,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [\App\Http\Controllers\JobsController::class, 'index'])->name('jobs.index');
 
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('search', [\App\Http\Controllers\JobsController::class, 'search']);
 
 Route::group(['prefix' => 'city'], function () {
     Route::get('/', [\App\Http\Controllers\CitiesController::class, 'index'])->name('cities.index');
@@ -28,4 +28,29 @@ Route::group(['prefix' => 'city'], function () {
     Route::get('edit/{id}', [\App\Http\Controllers\CitiesController::class, 'edit'])->name('cities.edit');
     Route::put('update/{id}', [\App\Http\Controllers\CitiesController::class, 'update'])->name('cities.update');
     Route::delete('/delete/{id}', [\App\Http\Controllers\CitiesController::class, 'delete'])->name('cities.delete');
+});
+Route::group(['prefix' => 'position'], function () {
+    Route::get('/', [\App\Http\Controllers\PositionsController::class, 'index'])->name('positions.index');
+    Route::get('/create', [\App\Http\Controllers\PositionsController::class, 'create'])->name('positions.create');
+    Route::post('/', [\App\Http\Controllers\PositionsController::class, 'store'])->name('positions.store');
+    Route::get('edit/{id}', [\App\Http\Controllers\PositionsController::class, 'edit'])->name('positions.edit');
+    Route::put('update/{id}', [\App\Http\Controllers\PositionsController::class, 'update'])->name('positions.update');
+    Route::delete('/delete/{id}', [\App\Http\Controllers\PositionsController::class, 'delete'])->name('positions.delete');
+});
+Route::group(['prefix' => 'job'], function () {
+    Route::get('/', [\App\Http\Controllers\JobsController::class, 'index'])->name('jobs.index');
+    Route::get('/admin', [\App\Http\Controllers\JobsController::class, 'index_admin'])->name('jobs.index_admin');
+    Route::get('/create', [\App\Http\Controllers\JobsController::class, 'create'])->name('jobs.create');
+    Route::post('/', [\App\Http\Controllers\JobsController::class, 'store'])->name('jobs.store');
+    Route::get('edit/{id}', [\App\Http\Controllers\JobsController::class, 'edit'])->name('jobs.edit');
+    Route::put('update/{id}', [\App\Http\Controllers\JobsController::class, 'update'])->name('jobs.update');
+    Route::delete('/delete/{id}', [\App\Http\Controllers\JobsController::class, 'delete'])->name('jobs.delete');
+});
+Route::group(['prefix' => 'company'], function () {
+    Route::get('/', [\App\Http\Controllers\CompaniesController::class, 'index'])->name('companies.index');
+    Route::get('/create', [\App\Http\Controllers\CompaniesController::class, 'create'])->name('companies.create');
+    Route::post('/', [\App\Http\Controllers\CompaniesController::class, 'store'])->name('companies.store');
+    Route::get('edit/{id}', [\App\Http\Controllers\CompaniesController::class, 'edit'])->name('companies.edit');
+    Route::put('update/{id}', [\App\Http\Controllers\CompaniesController::class, 'update'])->name('companies.update');
+    Route::delete('/delete/{id}', [\App\Http\Controllers\CompaniesController::class, 'delete'])->name('companies.delete');
 });
